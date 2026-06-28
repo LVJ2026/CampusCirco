d9f5eb66db774932e59234096398a01a93d95ba5
LVJ2026 <Laurence.Virbel-Jeandel@ac-nancy-metz.fr>
2026-06-28 15:43:14 +0200

    Update grist.js

--- js/grist.js
+++ js/grist.js
@@ -1,15 +1,8 @@
 async function testerConnexionGrist() {
 
-    const url =
-        `https://grist.numerique.gouv.fr/api/docs/${CONFIG.DOCUMENT_ID}/tables`;
-
     try {
 
-        const reponse = await fetch(url, {
-            headers: {
-                Authorization: `Bearer ${CONFIG.API_KEY}`
-            }
-        });
+        const reponse = await fetch("/.netlify/functions/grist");
 
         if (!reponse.ok) {
             throw new Error("Erreur " + reponse.status);
@@ -20,8 +13,7 @@ async function testerConnexionGrist() {
         console.log("Connexion réussie !");
         console.log(tables);
 
-    }
-    catch (erreur) {
+    } catch (erreur) {
 
         console.error("Erreur Grist :", erreur);
 
